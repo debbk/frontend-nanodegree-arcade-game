12894954f2c5f150eca80ab3be43f35089b1a66a
Deborah King <debbie.khan@gmail.com>
2015-09-13 20:24:04 -0400

    JS file run in beautifier

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ var BOUNDY_BOTTOM = 450;
 var BOUNDX_RIGHT = 410;
 var BOUNDX_LEFT = 0;
 //Player starting position
-var PLAYER_X = 205;  //canvas width is 505
-var PLAYER_Y = 400;  //canvas height is 606
+var PLAYER_X = 205; //canvas width is 505
+var PLAYER_Y = 400; //canvas height is 606
 //Movement distances
 var HORIZONTAL = 100;
 var VERTICAL = 85;
@@ -25,19 +25,19 @@ var OBSTRUCT_WIDTH = 100;
 var OBSTRUCT_HEIGHT = 60;
 
 /**************** Game Status Code ****************/
-var Game = function () {
+var Game = function() {
     this.endGame = false;
     this.winGame = false;
     this.restart = false;
 };
 
 //Check status of game
-Game.prototype.update = function () {
+Game.prototype.update = function() {
     this.win();
 };
 
 //Check if player has won
-Game.prototype.win = function () {
+Game.prototype.win = function() {
     if (player.x === 305 && player.y === -25) {
         this.winGame = true;
     }
@@ -47,7 +47,7 @@ Game.prototype.win = function () {
 var newGame = new Game();
 
 /**************** Item Code ****************/
-var Item = function () {
+var Item = function() {
     this.item = true;
     this.x = ITEM_X;
     this.y = ITEM_Y;
@@ -56,27 +56,27 @@ var Item = function () {
 };
 
 //Draw the item on the screen
-Item.prototype.render = function () {
+Item.prototype.render = function() {
     if (this.loc === 'present') {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 };
 
 //Check status of item
-Item.prototype.update = function () {
+Item.prototype.update = function() {
     this.statusCheck();
 };
 
 //If player touches item, remove item
-Item.prototype.statusCheck = function () {
+Item.prototype.statusCheck = function() {
     if (this.collide() === true) {
         this.loc = 'remove';
     }
 };
 
 //Check for collisions with items
-Item.prototype.collide = function () {
-    if (this.x <player.x + ITEM_WIDTH && this.x > player.x - ITEM_WIDTH && this.y < player.y + ITEM_HEIGHT && this.y >player.y - ITEM_HEIGHT) {
+Item.prototype.collide = function() {
+    if (this.x < player.x + ITEM_WIDTH && this.x > player.x - ITEM_WIDTH && this.y < player.y + ITEM_HEIGHT && this.y > player.y - ITEM_HEIGHT) {
         return true;
     }
 };
@@ -85,7 +85,7 @@ Item.prototype.collide = function () {
 var item = new Item();
 
 /**************** Obstruction Code ****************/
-var Obstruction = function () {
+var Obstruction = function() {
     this.x = OBSTRUCT_X;
     this.y = OBSTRUCT_Y;
     this.sprite = 'images/Rock.png';
@@ -93,26 +93,26 @@ var Obstruction = function () {
 };
 
 //Draw the item on the screen
-Obstruction.prototype.render = function () {
+Obstruction.prototype.render = function() {
     if (this.loc === 'present') {
-       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 };
 
 //Check status of obstruction
-Obstruction.prototype.update = function () {
+Obstruction.prototype.update = function() {
     this.statusCheck();
 };
 
 //If player touches obstruction, remove obstruction
-Obstruction.prototype.statusCheck = function () {
+Obstruction.prototype.statusCheck = function() {
     if (item.collide() === true) {
         this.loc = 'remove';
     }
 };
 
 //Check if barrier is present
-Obstruction.prototype.barrier = function () {
+Obstruction.prototype.barrier = function() {
     if (this.loc === 'present') {
         return true;
     } else {
@@ -125,7 +125,7 @@ var obstruction = new Obstruction();
 
 /**************** Enemy Code ****************/
 // Enemies our player must avoid
-var Enemy = function (locx, locy, speed) {
+var Enemy = function(locx, locy, speed) {
     this.x = locx;
     this.y = locy;
     this.speed = speed;
@@ -137,7 +137,7 @@ var Enemy = function (locx, locy, speed) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
+Enemy.prototype.update = function(dt) {
     this.location();
     this.move(dt);
     this.pic();
@@ -145,32 +145,32 @@ Enemy.prototype.update = function (dt) {
 
 //Assign a direction to the enemies depending on their x location
 //also assign an image to the enemy depending on their movement direction
-Enemy.prototype.location = function (dt) {
-    if (this.y < 70 && obstruction.barrier() === true){
-        if(this.x > 200) {
+Enemy.prototype.location = function(dt) {
+    if (this.y < 70 && obstruction.barrier() === true) {
+        if (this.x > 200) {
             this.direction = "left";
-        } else if (this.x < BOUNDX_LEFT){
+        } else if (this.x < BOUNDX_LEFT) {
             this.direction = "right";
         }
     }
     if (this.x > BOUNDX_RIGHT) {
         this.direction = "left";
-    } else if (this.x < BOUNDX_LEFT){
+    } else if (this.x < BOUNDX_LEFT) {
         this.direction = "right";
     }
 };
 
 //Assign a speed to the enemies depending on their direction
-Enemy.prototype.move = function (dt) {
+Enemy.prototype.move = function(dt) {
     if (this.direction === "left") {
-        this.x -= (this.speed*dt);
+        this.x -= (this.speed * dt);
     } else if (this.direction === "right") {
-        this.x += (this.speed*dt);
+        this.x += (this.speed * dt);
     }
 };
 
 //Assign an image to the enemy depending on their direction
-Enemy.prototype.pic = function () {
+Enemy.prototype.pic = function() {
     "use strict";
     if (this.direction === "left") {
         this.sprite = 'images/enemy-bug-left.png';
@@ -189,7 +189,7 @@ var allEnemies = [];
 
 //Initiate enemies with random speeds
 for (var i = 0; i < 3; i++) {
-    var enemySpeed=getRandomInt(100,300);
+    var enemySpeed = getRandomInt(100, 300);
     //Randomize speed of enemies
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -199,18 +199,17 @@ for (var i = 0; i < 3; i++) {
 
 /**************** Player Code ****************/
 //Create player class
-var Player = function () {
+var Player = function() {
     this.x = PLAYER_X;
     this.y = PLAYER_Y;
     this.sprite = "images/char-horn-girl.png";
 };
 
 //Update player's position
-Player.prototype.update = function (dt) {
-};
+Player.prototype.update = function(dt) {};
 
 //Draw the player on the screen if game is not over
-Player.prototype.render = function () {
+Player.prototype.render = function() {
     if (newGame.endGame === false) {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
@@ -220,81 +219,75 @@ Player.prototype.render = function () {
 //Only allow player to move if the game hasn't ended
 if (newGame.endGame === false && newGame.winGame === false) {
     // if movement is within the bounds of the map
-    Player.prototype.handleInput = function (keycode) {
+    Player.prototype.handleInput = function(keycode) {
         switch (keycode) {
-        case "left":
-            //*Barrier Present
-            //Stop from waking into barrier if barrier is present
-            if (obstruction.barrier() === true && this.x === (OBSTRUCT_X + OBSTRUCT_WIDTH) && this.y === OBSTRUCT_Y) {
-            //Stay in bounds of screen
-            } else if (obstruction.barrier() === true && this.x - HORIZONTAL > BOUNDX_LEFT && (this.y !== OBSTRUCT_Y || this.x !== (OBSTRUCT_X + OBSTRUCT_WIDTH))) {
-                this.x -= HORIZONTAL;
-            //*Barrier not present
-            //Stay in map bounds
-            } else if (obstruction.barrier() === false && this.x - HORIZONTAL < BOUNDX_LEFT) {
-            //Do not walk into water
-            } else if (obstruction.barrier() === false && this.x === OBSTRUCT_X && this.y < OBSTRUCT_Y) {
-            } else if (obstruction.barrier() === false && this.x - HORIZONTAL > BOUNDX_LEFT) {
-                this.x -= HORIZONTAL;
-            }
-            break;
-        case "right":
-            //*Barrier Present//
-            //Stay in map bounds
-            if (obstruction.barrier() === true && this.x + HORIZONTAL > BOUNDX_RIGHT) {
-            //Do not walk into barrier
-            } else if (obstruction.barrier() === true && this.x === (OBSTRUCT_X - OBSTRUCT_WIDTH) && this.y === OBSTRUCT_Y) {
-            //Allow movement if barrier not in the way
-            } else if (obstruction.barrier() === true && (this.x !== (OBSTRUCT_X - OBSTRUCT_WIDTH) || this.y !== OBSTRUCT_Y)) {
-                this.x += HORIZONTAL;
-            //*Barrier Removed//
-            //Stay in map bounds
-            } else if (obstruction.barrier() === false && this.x + HORIZONTAL > BOUNDX_RIGHT) {
-            //Do not walk in water
-            } else if (obstruction.barrier() === false && this.x + HORIZONTAL > (OBSTRUCT_X - OBSTRUCT_WIDTH) && this.y < OBSTRUCT_Y) {
-            } else if (obstruction.barrier() === false && this.x + HORIZONTAL < BOUNDX_RIGHT) {
-                this.x += HORIZONTAL;
-            }
-            break;
-        case "up":
-            //*Barrier Present//
-            //Stop from walking into barrier if barrier is present
-            if (this.x === (OBSTRUCT_X) && obstruction.barrier() === true && this.y - VERTICAL < (OBSTRUCT_Y + OBSTRUCT_HEIGHT)) {
-            } else if (this.x === (OBSTRUCT_X) && obstruction.barrier() === true && this.y.VERTICAL > (OBSTRUCT_Y + OBSTRUCT_HEIGHT) && this.y - VERTICAL < BOUNDY_TOP) {
-            //prevent player from walking into water
-            } else if (this.x !== (OBSTRUCT_X) && obstruction.barrier() === true && this.y - VERTICAL < OBSTRUCT_Y) {
-            //***Barrier Removed//
-            //Allow player to walk upwards if barrier is removed
-            } else if (this.x === (OBSTRUCT_X) && obstruction.barrier() === false && this.y - VERTICAL < BOUNDY_TOP) {
-            } else if (this.x !== (OBSTRUCT_X) && obstruction.barrier() === false && this.y - VERTICAL < OBSTRUCT_Y) {
-            } else {
-                this.y -= VERTICAL;
-            }
-            break;
-        case "down":
-            if (this.y + VERTICAL > BOUNDY_BOTTOM) {
-            } else {
-                this.y += VERTICAL;
-            }
-            break;
-        case "space":
-            //Reset player status
-            if (newGame.endGame === true) {
-                player.reset();
-                obstruction.loc = "present";
-                item.loc = "present";
-            } else if (newGame.winGame === true) {
-                player.reset();
-                obstruction.loc = "present";
-                item.loc = "present";
-            }
-            break;
+            case "left":
+                //*Barrier Present
+                //Stop from waking into barrier if barrier is present
+                if (obstruction.barrier() === true && this.x === (OBSTRUCT_X + OBSTRUCT_WIDTH) && this.y === OBSTRUCT_Y) {
+                    //Stay in bounds of screen
+                } else if (obstruction.barrier() === true && this.x - HORIZONTAL > BOUNDX_LEFT && (this.y !== OBSTRUCT_Y || this.x !== (OBSTRUCT_X + OBSTRUCT_WIDTH))) {
+                    this.x -= HORIZONTAL;
+                    //*Barrier not present
+                    //Stay in map bounds
+                } else if (obstruction.barrier() === false && this.x - HORIZONTAL < BOUNDX_LEFT) {
+                    //Do not walk into water
+                } else if (obstruction.barrier() === false && this.x === OBSTRUCT_X && this.y < OBSTRUCT_Y) {} else if (obstruction.barrier() === false && this.x - HORIZONTAL > BOUNDX_LEFT) {
+                    this.x -= HORIZONTAL;
+                }
+                break;
+            case "right":
+                //*Barrier Present//
+                //Stay in map bounds
+                if (obstruction.barrier() === true && this.x + HORIZONTAL > BOUNDX_RIGHT) {
+                    //Do not walk into barrier
+                } else if (obstruction.barrier() === true && this.x === (OBSTRUCT_X - OBSTRUCT_WIDTH) && this.y === OBSTRUCT_Y) {
+                    //Allow movement if barrier not in the way
+                } else if (obstruction.barrier() === true && (this.x !== (OBSTRUCT_X - OBSTRUCT_WIDTH) || this.y !== OBSTRUCT_Y)) {
+                    this.x += HORIZONTAL;
+                    //*Barrier Removed//
+                    //Stay in map bounds
+                } else if (obstruction.barrier() === false && this.x + HORIZONTAL > BOUNDX_RIGHT) {
+                    //Do not walk in water
+                } else if (obstruction.barrier() === false && this.x + HORIZONTAL > (OBSTRUCT_X - OBSTRUCT_WIDTH) && this.y < OBSTRUCT_Y) {} else if (obstruction.barrier() === false && this.x + HORIZONTAL < BOUNDX_RIGHT) {
+                    this.x += HORIZONTAL;
+                }
+                break;
+            case "up":
+                //*Barrier Present//
+                //Stop from walking into barrier if barrier is present
+                if (this.x === (OBSTRUCT_X) && obstruction.barrier() === true && this.y - VERTICAL < (OBSTRUCT_Y + OBSTRUCT_HEIGHT)) {} else if (this.x === (OBSTRUCT_X) && obstruction.barrier() === true && this.y.VERTICAL > (OBSTRUCT_Y + OBSTRUCT_HEIGHT) && this.y - VERTICAL < BOUNDY_TOP) {
+                    //prevent player from walking into water
+                } else if (this.x !== (OBSTRUCT_X) && obstruction.barrier() === true && this.y - VERTICAL < OBSTRUCT_Y) {
+                    //***Barrier Removed//
+                    //Allow player to walk upwards if barrier is removed
+                } else if (this.x === (OBSTRUCT_X) && obstruction.barrier() === false && this.y - VERTICAL < BOUNDY_TOP) {} else if (this.x !== (OBSTRUCT_X) && obstruction.barrier() === false && this.y - VERTICAL < OBSTRUCT_Y) {} else {
+                    this.y -= VERTICAL;
+                }
+                break;
+            case "down":
+                if (this.y + VERTICAL > BOUNDY_BOTTOM) {} else {
+                    this.y += VERTICAL;
+                }
+                break;
+            case "space":
+                //Reset player status
+                if (newGame.endGame === true) {
+                    player.reset();
+                    obstruction.loc = "present";
+                    item.loc = "present";
+                } else if (newGame.winGame === true) {
+                    player.reset();
+                    obstruction.loc = "present";
+                    item.loc = "present";
+                }
+                break;
         }
     };
 }
 
 //Reset player position and game status
-Player.prototype.reset = function () {
+Player.prototype.reset = function() {
     this.x = PLAYER_X;
     this.y = PLAYER_Y;
     newGame.endGame = false;
@@ -302,8 +295,8 @@ Player.prototype.reset = function () {
 };
 
 //*******  Check for collisions
-Player.prototype.collide = function () {
-    for(var i = 0; i < allEnemies.length; i++) {
+Player.prototype.collide = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
         if (this.x < allEnemies[i].x + ENEMY_WIDTH && this.x > allEnemies[i].x - ENEMY_WIDTH && this.y < allEnemies[i].y + ENEMY_HEIGHT && this.y > allEnemies[i].y - ENEMY_HEIGHT) {
             return true;
         }
@@ -316,7 +309,7 @@ var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener ('keyup', function(e) {
+document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
